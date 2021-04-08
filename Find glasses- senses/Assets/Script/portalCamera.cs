51f72fa86250a133ab1d28b9d16b8b5c54cd8afe
Dianhanluosi <evanzheng1127@gmail.com>
2021-04-08 04:19:40 -0700

    Portal fix

--- Find glasses- senses/Assets/Script/portalCamera.cs
+++ Find glasses- senses/Assets/Script/portalCamera.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class PortalCamera : MonoBehaviour
 {
-    public Transform playerCamera;
-    public Transform portal;
-    public Transform otherPortal;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
-        transform.position = portal.position + playerOffsetFromPortal;
+	public Transform playerCamera;
+	public Transform portal;
+	public Transform otherPortal;
 
-        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+		transform.position = portal.position + playerOffsetFromPortal;
 
-        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
-        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
-        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
-    }
+		float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+		Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+		Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+		transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+	}
 }
